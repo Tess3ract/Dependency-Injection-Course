af4d81f7fb0ee4630592835f25c71c80b844f52e
Moritz <moritz.niederer@stud-mail.uni-wuerzburg.de>
2022-01-06 11:45:46 +0100

    initial state from initializr

---
+++ src/main/java/moritzniederer/DependencyInjection/DependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package moritzniederer.DependencyInjection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependencyInjectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moritzniederer/DependencyInjection/DependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package moritzniederer.DependencyInjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
